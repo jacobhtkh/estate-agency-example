d2610ead61f15dc96bc6f0329786fc25f02b3774
Jacob Ujanga <jacobujanga@gmail.com>
2021-03-10 11:44:10 +0000

    Simplify landing page styles

--- src/pages/landing-page/landing-page.component.jsx
+++ src/pages/landing-page/landing-page.component.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import {
   LandingPageContainer,
-  LandingPageImageContainer,
   LandingPageImage,
   LandingPageTitle,
 } from './landing-page.styles';
@@ -9,13 +8,11 @@ import {
 const LandingPage = (props) => {
   return (
     <LandingPageContainer>
-      <LandingPageImageContainer>
-        <LandingPageImage
-          src={
-            'https://images.unsplash.com/photo-1575386848021-028a496c03fe?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80'
-          }
-        />
-      </LandingPageImageContainer>
+      <LandingPageImage
+        src={
+          'https://images.unsplash.com/photo-1575386848021-028a496c03fe?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80'
+        }
+      />
       <LandingPageTitle>Find Your Dream Home</LandingPageTitle>
     </LandingPageContainer>
   );
--- src/pages/landing-page/landing-page.styles.jsx
+++ src/pages/landing-page/landing-page.styles.jsx
@@ -1,16 +1,10 @@
 import styled from 'styled-components';
 
 export const LandingPageContainer = styled.div`
-  width: 100%;
-  height: 100%;
   display: flex;
+  flex: 1;
   flex-direction: column;
   align-items: center;
-`;
-
-export const LandingPageImageContainer = styled.div`
-  width: 100%;
-  display: flex;
 `;
 
 export const LandingPageImage = styled.img`
